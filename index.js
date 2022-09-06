ca9efd9a5b539659cc134b3e5a810292092f5703
George Fernandez <fernandezg704@gmail.com>
2022-09-06 13:32:21 -0400

    commit tree 91891befda0bce59b1961112111586349131a8d0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`)
+// }
+
+function writeCards (name, event = 'surprise') {
+    let myArray = []
+    for (let names of name) {  
+        myArray.push (`Thank you, ${names}, for the wonderful ${event} gift!`);
+    }
+    return myArray
+}
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num)
+      num--
+    }
+  }
